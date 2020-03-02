14447f1d3dcfe765d5b6f288af0ef863ff7abd0b
Alice <alice.easey@gmail.com>
2020-03-02 14:44:45 +0000

    Added my solution for 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo phrase
+    return phrase
+end
+
+def shout phrase
+    return phrase.upcase
+end
+
+def repeat phrase, num_times = 2
+    str = ""
+    (1..num_times).each { str = phrase + ' ' + str}
+    return str.strip
+end
+
+def start_of_word word, index
+    return word.slice(0, index)
+end
+
+def first_word phrase
+    index = phrase.index(' ')
+    return phrase.slice(0, index)
+end
+
+def titleize phrase
+    arr = phrase.split(' ')
+    arr.each_with_index do |word, i|
+        if(i === 0 || !(word === 'and' || word === 'of' || word === 'over' || word === 'the')) 
+            word.capitalize!
+        end
+    end
+    return arr.join(' ')
+end
