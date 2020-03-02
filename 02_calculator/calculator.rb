5742968641a775b24ada726f0a789143a8df76cd
Alice <alice.easey@gmail.com>
2020-03-02 13:39:23 +0000

    Added my solution to 03_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add x, y
+    x + y
+end
+
+def subtract x, y
+    x - y
+end
+
+def sum arr
+    total = 0
+    arr.each {|i| total = total + i}
+    return total
+end
+
+def multiply x, *rest
+    total = x
+    rest.each {|i| total = total * i}
+    return total
+end
+
+def power x, y
+    x ** y
+end
+
+def factorial x
+    if(x === 0)
+        return 1
+    end
+    
+    total = 1
+    while(x > 0) do
+        total = total * x
+        x = x - 1
+    end
+    return total
+end
