109616757702be9b927a4ed6081c7d2127c3e31a
Alice <alice.easey@gmail.com>
2020-03-03 18:35:46 +0000

    Added my solution for 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
-#write your code here
+def translate phrase
+    words = phrase.split(' ')
+    result = ""
+    words.each do |word|
+        first_three_letters = word.slice(0, 3)
+        puts first_three_letters
+        num_consonants = first_three_letters =~ /qu/
+        
+        if(num_consonants)
+            num_consonants = first_three_letters.slice(0, num_consonants+2).length
+        else
+            num_consonants = first_three_letters[/\A[a-z&&[^aeiou]]*/i].length
+        end
+
+        result = result + ' ' + word.slice(num_consonants, word.length) + word.slice(0, num_consonants) + 'ay'
+    end
+    return result.strip
+end
+
--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -67,6 +67,14 @@ describe "#translate" do
 
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
+  it "capitalises words" do
+    s = translate("Donald Duck")
+    expect(s).to eq("Onaldday Uckday")
+  end
   # * retain the punctuation from the original phrase
+  it "retains punctuation" do
+    s = translate("Hooray! We're going to Barbados!")
+    expect(s).to eq("Oorayhay! E'reway oinggay otay Arbadosbay!")
+  end
 
 end
