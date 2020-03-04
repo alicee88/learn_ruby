f7e1f2aaaed04eb928082f9bfc33a705168d5fef
Alice <alice.easey@gmail.com>
2020-03-04 09:39:23 +0000

    Added my solution for 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,13 @@
 class Book
 # write your code here
+    attr_writer :title
+
+    def title
+        @title.split(' ').each.with_index do |word, index|
+            if (! word.match(/^(and|the|of|in|a|an)$/)) || index === 0
+                word.capitalize!
+            end
+        end.join(' ')
+
+    end
 end
