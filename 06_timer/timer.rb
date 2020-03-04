3311ad222f023ba28eebb59178d06672baeb1804
Alice <alice.easey@gmail.com>
2020-03-04 09:55:27 +0000

    Added my solution for 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,28 @@
 class Timer
   #write your code here
+  def seconds=(seconds)
+      @seconds = seconds
+  end
+ 
+  def seconds
+    @seconds = 0
+  end
+
+  def time_string
+    secs = @seconds % 60
+    mins = (@seconds / 60).floor % 60
+    hours = (@seconds / 60 / 60).floor % 60
+
+    if(hours < 10)
+      hours = '0'+hours.to_s
+    end
+    if(mins < 10)
+      mins = '0'+mins.to_s
+    end
+    if(secs < 10)
+      secs = '0'+secs.to_s
+    end
+    return "#{hours}:#{mins}:#{secs}"
+  end
+
 end
